009f3403cb32ce7ff02a30935d85a066755d9138
mochico mochizuki <mmochi2999@gmail.com>
2015-11-27 00:53:31 +0900

    fix directory

--- app/src/main/java/com/kayac/omoshiroapp/activity/MainActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kayac.omoshiroapp;
+package com.kayac.omoshiroapp.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.kayac.omoshiroapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/kayac/omoshiroapp/activity/SugoiActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/SugoiActivity.java
@@ -1,4 +1,4 @@
-package com.kayac.omoshiroapp;
+package com.kayac.omoshiroapp.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.kayac.omoshiroapp.R;
 
 public class SugoiActivity extends AppCompatActivity {
 
