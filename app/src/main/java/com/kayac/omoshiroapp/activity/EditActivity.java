fb74939493b2a895c9ea243fdf709a7903fe22ad
devmassive <hosono-kyohei@kayac.com>
2015-12-03 20:55:58 +0900

    カメラで撮影した写真をファイルに書き出す

--- app/src/main/java/com/kayac/omoshiroapp/activity/EditActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/EditActivity.java
@@ -2,6 +2,8 @@ package com.kayac.omoshiroapp.activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,11 @@ import android.widget.ImageView;
 
 import com.kayac.omoshiroapp.R;
 
+import java.io.File;
+
 public class EditActivity extends AppCompatActivity {
+
+    File mImageFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,10 @@ public class EditActivity extends AppCompatActivity {
         photoFrame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                File storageDir = getExternalFilesDir(null);
+                mImageFile = new File(storageDir, "book.jpg");
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mImageFile));
                 startActivityForResult(intent, 1192);
             }
         });
@@ -32,7 +41,8 @@ public class EditActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Bitmap bitmap = data.getParcelableExtra("data");
+        // Bitmap bitmap = data.getParcelableExtra("data");
+        Bitmap bitmap = BitmapFactory.decodeFile(mImageFile.getAbsolutePath());
         ImageView photoThumbnail = (ImageView) findViewById(R.id.attachment_photo_thumbnail);
         photoThumbnail.setImageBitmap(bitmap);
     }
