20f8f8f89eda1db7cacf9cd5f3e7f96b0374b951
devmassive <hosono-kyohei@kayac.com>
2015-12-03 20:41:28 +0900

    カメラを起動してサムネイルを表示する

--- app/src/main/java/com/kayac/omoshiroapp/activity/EditActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/EditActivity.java
@@ -1,7 +1,13 @@
 package com.kayac.omoshiroapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.kayac.omoshiroapp.R;
 
@@ -11,5 +17,23 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        FrameLayout photoFrame = (FrameLayout) findViewById(R.id.attachment_photo_frame);
+        photoFrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1192);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = data.getParcelableExtra("data");
+        ImageView photoThumbnail = (ImageView) findViewById(R.id.attachment_photo_thumbnail);
+        photoThumbnail.setImageBitmap(bitmap);
     }
 }
