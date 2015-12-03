6fd61b543cb1ccc239d6bf6bf20c03a92f57802d
devmassive <hosono-kyohei@kayac.com>
2015-12-03 20:24:24 +0900

    MainActivityからDetailActivityへの遷移

--- app/src/main/java/com/kayac/omoshiroapp/activity/DetailActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/DetailActivity.java
@@ -1,8 +1,8 @@
 package com.kayac.omoshiroapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +10,9 @@ import android.view.View;
 import com.kayac.omoshiroapp.R;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // 定数にしておくと打ち間違える心配がないよ
+    public static final String EXTRA_BOOK_ID = "EXTRA_BOOK_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,10 @@ public class DetailActivity extends AppCompatActivity {
 
         // これでToolBarに戻るボタン（左矢印）がでるのだ
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String bookId = getIntent().getStringExtra(EXTRA_BOOK_ID);
+        setTitle("これは本" + bookId + "だ");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     }
-
 }
--- app/src/main/java/com/kayac/omoshiroapp/activity/MainActivity.java
+++ app/src/main/java/com/kayac/omoshiroapp/activity/MainActivity.java
@@ -1,13 +1,15 @@
 package com.kayac.omoshiroapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.kayac.omoshiroapp.R;
 
@@ -26,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button buttonBook1 = (Button) findViewById(R.id.button_book1);
+        buttonBook1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.EXTRA_BOOK_ID, "1");
+                startActivity(intent);
+            }
+        });
+
+        Button buttonBook2 = (Button) findViewById(R.id.button_book2);
+        buttonBook2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.EXTRA_BOOK_ID, "2");
+                startActivity(intent);
             }
         });
     }
